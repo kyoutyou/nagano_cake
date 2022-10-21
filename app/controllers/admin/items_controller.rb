0df65735c1a426eed52e62c5abc7572b8311f5b8
EC2 Default User <ec2-user@ip-172-31-1-137.ap-northeast-1.compute.internal>
2022-10-21 15:23:16 +0000

    [Add]会員とお店側のヘッダー部分のリンク

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,19 @@
 class Admin::ItemsController < ApplicationController
   def index
-   @items=Item.all 
+    @items=Item.all 
   end
   
   def new
+    @item=Item.new
   end  
   
   def show
     @item=Item.find(params[:id])
+  end
+  
+  def create
+    item=Item.new
+    item.save
+    redirect_to admin_item_path
   end
 end
